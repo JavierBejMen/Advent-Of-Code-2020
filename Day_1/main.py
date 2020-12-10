ed37bb5eab696fe96da4cbf6575bc74ffedf4d30
Javier Bejar <j.bejar.mendez@gmail.com>
2020-12-10 18:51:58 +0100

    Pretied

--- Day_1/main.py
+++ Day_1/main.py
@@ -1,30 +1,22 @@
 from collections import OrderedDict
-import traceback
 
 def main():
-    try:
-        file = open("../Day_1/input.txt", "r")
-
+    with open("input.txt", "r") as file:
         numbers = list(map(lambda line: int(line), file.read().splitlines()))
-        ocurrences = OrderedDict()
-        target = 2020
+    
+    ocurrences = OrderedDict()
+    target = 2020
 
-        for i in numbers:
-            for j in numbers:
-                if target - i - j in ocurrences:
-                    print("{0} * {1} * {2} = {3}".format(i, j, target-i-j, i * j * (target-i-j)))
-                    file.close()
-                    return 0
-                else:
-                    ocurrences[j] = None
+    for i in numbers:
+        for j in numbers:
+            if target - i - j in ocurrences:
+                print("{0} * {1} * {2} = {3}".format(i, j, target-i-j, i * j * (target-i-j)))
+                return 0
+            else:
+                ocurrences[j] = None
 
-        print("Fail")
-        file.close()
-        return 1
-
-    except:
-        traceback.print_exc()
-        return 1
+    print("Fail")
+    return 1
 
 if __name__ == "__main__":
     main()
